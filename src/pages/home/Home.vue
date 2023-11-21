eea440eb156dd1459801328690a9a1625309f14f
zhangruiquan@genew.com <Nucleus!>
2023-11-21 19:11:52 +0800

    完成首页图标展示

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,20 @@
   <div>
     <home-header></home-header>
     <home-swiper></home-swiper>
+    <home-icons></home-icons>
   </div>
 </template>
 
 <script>
 import HomeHeader from './conponents/Header.vue'
 import HomeSwiper from './conponents/Swiper.vue'
+import HomeIcons from './conponents/Icons.vue'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    HomeSwiper
+    HomeSwiper,
+    HomeIcons
   }
 }
 </script>
