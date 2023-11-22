b92ccc29ff3b83b7acdaab294b4e4e2433b573b3
zhangruiquan@genew.com <Nucleus!>
2023-11-22 15:35:06 +0800

    完成热销推荐和周末游组件的开发

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,6 +3,8 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-recommend></home-recommend>
+    <home-weekend></home-weekend>
   </div>
 </template>
 
@@ -10,12 +12,16 @@
 import HomeHeader from './conponents/Header.vue'
 import HomeSwiper from './conponents/Swiper.vue'
 import HomeIcons from './conponents/Icons.vue'
+import HomeRecommend from './conponents/Recommend.vue'
+import HomeWeekend from './conponents/Weekend.vue'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
   }
 }
 </script>
