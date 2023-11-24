04ee657c41d9c2975d245ccf9dcd0058cf5c7a48
zhangruiquan@genew.com <Nucleus!>
2023-11-24 17:07:10 +0800

    完成城市搜索框

---
+++ src/pages/city/conponents/Search.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="search">
+        <input class="searchInput" type="text" placeholder=输入城市名或拼音>
+    </div>
+  </template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+@import '~styles/mixins.styl'
+.search
+  height: .72rem
+  padding: 0 .1rem
+  background: $bgColor
+  .searchInput
+    box-sizing: border-box
+    width: 100%
+    height: .62rem
+    line-height: .62rem
+    text-align: center
+    border-radius: .06rem
+    color: #666
+    padding: 0 .1rem
+
+</style>
