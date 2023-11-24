740eefe54c350c669b78fe875a4996ea24866e18
zhangruiquan@genew.com <Nucleus!>
2023-11-24 15:42:21 +0800

    城市选择页面路由配置及页头开发

---
+++ src/pages/city/City.vue
@@ -0,0 +1,19 @@
+<template>
+  <city-header></city-header>
+</template>
+
+<script>
+import CityHeader from './conponents/Header'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+@import '~styles/mixins.styl'
+
+</style>
--- src/pages/home/conponents/Header.vue
+++ src/pages/home/conponents/Header.vue
@@ -7,10 +7,12 @@
         <span class="iconfont">&#xe632;</span>
         输入城市/景点/游玩主题
       </div>
+      <router-link to="/city">
       <div class="header-right">
         {{ this.city }}
         <span class="iconfont arrow-icon">&#xe62d;</span>
       </div>
+    </router-link>
     </div>
   </template>
 
@@ -36,7 +38,7 @@ export default {
 @import '~styles/varibles.styl'
 .header
   display: flex
-  line-height: .86rem
+  line-height: $headerHeight
   background: $bgColor
   color: #fff
   .header-left
@@ -59,6 +61,7 @@ export default {
     width: 1.24rem
     float: right
     text-align: center
+    color: #fff
     .arrow-icon
       margin-left: -.04rem
       font-size: .24rem
