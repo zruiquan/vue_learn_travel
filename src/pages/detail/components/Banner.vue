7a844fbcf8b6fd57c70927cb78d49867a61c33f8
zhangruiquan@genew.com <Nucleus!>
2023-12-22 15:17:45 +0800

    加入基础动画效果

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -10,16 +10,19 @@
         </div>
       </div>
     </div>
-    <common-gallary
-     :imgs="gallaryImgs"
-     v-show="showGallary"
-     @close="handleGallaryClose"
-    ></common-gallary>
+    <fade-animation>
+      <common-gallary
+      :imgs="gallaryImgs"
+      v-show="showGallary"
+      @close="handleGallaryClose"
+      ></common-gallary>
+    </fade-animation>
   </div>
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
   props: {
@@ -28,7 +31,8 @@ export default {
     gallaryImgs: Array
   },
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   },
   data () {
     return {
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -9,11 +9,11 @@
 </template>
 
 <script>
-import HomeHeader from './components/Header.vue'
-import HomeSwiper from './components/Swiper.vue'
-import HomeIcons from './components/Icons.vue'
-import HomeRecommend from './components/Recommend.vue'
-import HomeWeekend from './components/Weekend.vue'
+import HomeHeader from './components/Header'
+import HomeSwiper from './components/Swiper'
+import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
 import axios from 'axios'
 import { mapState } from 'vuex'
 export default {
